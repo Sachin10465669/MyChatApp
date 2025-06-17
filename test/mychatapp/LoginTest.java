a0949db9502db5296d50a9ca7212c208bae6fe3e
Sachin10465669 <st10465669@rcconnect.edu.za>
2025-06-17 20:25:50 +0200

    updated code for part 3

--- test/mychatapp/LoginTest.java
+++ test/mychatapp/LoginTest.java
@@ -12,61 +12,61 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LoginTest {
 
     @Test
-    public void testValidUsername() {
+    void testValidUsername() {
         Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976");
         assertTrue(user.isUsernameValid());
     }
 
     @Test
-    public void testInvalidUsername() {
+    void testInvalidUsername() {
         Login user = new Login("kyle", "Ch&&sec@ke99!", "+27838968976");
         assertFalse(user.isUsernameValid());
     }
 
     @Test
-    public void testValidPassword() {
+    void testValidPassword() {
         Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976");
         assertTrue(user.isPasswordComplex());
     }
 
     @Test
-    public void testInvalidPassword() {
+    void testInvalidPassword() {
         Login user = new Login("kyl_1", "password", "+27838968976");
         assertFalse(user.isPasswordComplex());
     }
 
     @Test
-    public void testValidPhoneNumber() {
+    void testValidPhoneNumber() {
         Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976");
         assertTrue(user.isPhoneNumberValid());
     }
 
     @Test
-    public void testInvalidPhoneNumber() {
+    void testInvalidPhoneNumber() {
         Login user = new Login("kyl_1", "Ch&&sec@ke99!", "08966553");
         assertFalse(user.isPhoneNumberValid());
     }
 
     @Test
-    public void testRegistrationSuccessful() {
+    void testRegistrationSuccessful() {
         Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976");
         assertEquals("Registration successful.", user.registerUser());
     }
 
     @Test
-    public void testRegistrationInvalidUsername() {
+    void testRegistrationInvalidUsername() {
         Login user = new Login("kyle", "Ch&&sec@ke99!", "+27838968976");
         assertEquals("Username is not correctly formatted, please ensure that your username contains an underscore and is no more than five characters in length.", user.registerUser());
     }
 
     @Test
-    public void testRegistrationInvalidPassword() {
+    void testRegistrationInvalidPassword() {
         Login user = new Login("kyl_1", "password", "+27838968976");
         assertEquals("Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.", user.registerUser());
     }
 
     @Test
-    public void testRegistrationInvalidPhoneNumber() {
+    void testRegistrationInvalidPhoneNumber() {
         Login user = new Login("kyl_1", "Ch&&sec@ke99!", "08966553");
         assertEquals("Cell phone number is incorrectly formatted or does not contain international code.", user.registerUser());
     }
